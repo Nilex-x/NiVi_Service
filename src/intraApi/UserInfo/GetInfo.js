7709aca73f18095fb472ac8089933118b2dc3e20
nicolas <nicolas.moreau@epitech.eu>
2021-10-07 16:10:06 +0200

    Update getUserInfo Service

--- src/intraApi/UserInfo/GetInfo.js
+++ src/intraApi/UserInfo/GetInfo.js
@@ -2,6 +2,11 @@ const fetch = require('node-fetch')
 
 module.exports = {
     GetUserInfo: (KeyAuth) => {
-        
+        const url = `${process.env.API_INTRA}${KeyAuth}/planning/load?&format=json`;
+        console.log("entry url =>", url)
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log("data =>", data)
+        return 1;
     }
 }
