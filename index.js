cc5277e6ddef78bed8efe438571b135a072b283f
nicolas <nicolas.moreau@epitech.eu>
2021-10-07 14:32:25 +0200

    First push ADD server graphql

---
+++ index.js
@@ -0,0 +1,10 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./src/schema');
+const resolvers = require('./src/resolver')
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
